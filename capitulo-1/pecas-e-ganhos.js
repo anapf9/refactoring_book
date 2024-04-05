d447575b1056469a17ceb3334df2aeb953ff9749
Ana <ana.eng.dev@gmail.com>
2024-04-05 20:11:12 -0300

    ajuste para passar nos testes antes de transformar em classe

--- capitulo-1/pecas-e-ganhos.js
+++ capitulo-1/pecas-e-ganhos.js
@@ -3,6 +3,34 @@ export function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+  
+  function valorPara(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+  
+    return result
+  }
+  
+
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -27,29 +55,3 @@ export function statement(invoice, plays) {
   return result;
 }
 
-function playFor(aPerformance) {
-  return plays[aPerformance.playID]
-}
-
-function valorPara(aPerformance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-  }
-
-  return result
-}
